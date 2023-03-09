e56a77b5cfb2bf445decc12d089fe18f71e6e496
Maskie-Razzio <41023234@gm.nfu.edu.tw>
2023-03-09 14:58:35 +0800

    w3

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '', 'tags': '', 'url': 'About.html'}, {'title': 'w3', 'text': '', 'tags': '', 'url': 'w3.html'}]};
